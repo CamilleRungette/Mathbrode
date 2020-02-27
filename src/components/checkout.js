b553ccf2971c3e624a2a9b22b1fc5585ad434d76
CamilleRungette <c.rungette@gmail.com>
2020-02-27 18:24:30 +0100

    log2

--- src/components/checkout.js
+++ src/components/checkout.js
@@ -50,6 +50,8 @@ class CheckoutForm extends Component {
   }
 
   async persoOrderSubmit(ev){
+    console.log("helolololo2")
+
     let {token} = await this.props.stripe.createToken({name: "Name"});
     console.log(token)
     let response = await fetch(`${ip}/charge`, {
